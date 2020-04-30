18ff15a674b152942c09b3fd0354e60b7b664af6
grhicks593 <60280059+grhicks593@users.noreply.github.com>
2020-04-30 14:31:03 -0400

    Add files via upload

---
+++ Hello World/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+//Garett Hicks
+//Deliverable 1 - Hello World
+//Date: 4/28/2020
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // display "Hello World" message
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
